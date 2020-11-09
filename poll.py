d0272a1bf1229e56a4f8e58fb019dce12de7268b
jaxjags13 <christopher.dunlap32@gmail.com>
2020-11-09 13:50:18 -0600

    added 2020 polling as a csv, printed out each file, imported streamlit.

--- poll.py
+++ poll.py
@@ -1,6 +1,15 @@
 import pandas as pd
 import numpy as np
+import streamlit as st
 from sklearn.linear_model import LinearRegression
 
 full_training=pd.read_csv('2016_presidential_polls.csv')
+another_set1=pd.read_csv('2012-general-election-romney-vs-obama.csv')
+another_set2=pd.read_csv('presidential_polls_2020.csv')
+
 print(full_training)
+print(another_set1)
+print(another_set2)
+
+x = st.slider('x')
+st.write(x, 'squared is', x * x)
