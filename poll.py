710efec1fd0f22a22d9fc79287c01d2c57953fdb
Jay <jaythagar@gmail.com>
2020-11-22 15:03:52 -0600

    streamlit additions

--- poll.py
+++ poll.py
@@ -7,10 +7,10 @@ from matplotlib import cm
 from matplotlib.ticker import LinearLocator, FormatStrFormatter
 from matplotlib.colors import ListedColormap, LinearSegmentedColormap
 import matplotlib.pyplot as plt
+import altair as alt
 
 #another_set1=pd.read_csv('2012-general-election-romney-vs-obama.csv')
 #another_set2=pd.read_csv('presidential_polls_2020.csv')
-
 def decision_point(clf):
     coef = clf.coef_
     intercept = clf.intercept_
@@ -26,7 +26,40 @@ def to_float(x):
     x = float(x)
     return x
 
+# get dataset
 full_training=pd.read_csv('2016_sorted_polls.csv')
+
+####################################################
+### streamlit wrapper begins here###
+## streamlit is interspersed throughout the code
+st.title("Machine Learning for Poll Prediction")
+
+st.header("Poll Data used for testing")
+st.subheader("We used data from the 2016 presidential election to create a model to predict the outcome of the previous election")
+st.dataframe(full_training)
+
+st.header("Modeling and Testing")
+st.subheader("scatter matrix of dataset features")
+
+### scatter matrix ###
+#reset_index puts the index column back into the dataframe
+scatter_matrix = alt.Chart(full_training).mark_circle().encode(
+    alt.X(alt.repeat("column"), type='quantitative'),
+    alt.Y(alt.repeat("row"), type='quantitative'),
+    color='grade',size='samplesize',opacity=alt.value(0.4)
+).properties(
+    width=150,
+    height=150
+).repeat( #features can be added here to change matrix size and output
+    row=['actual_trump','rawpoll_trump','rawpoll_clinton','actual_clinton'],
+    column=['actual_trump','rawpoll_trump','rawpoll_clinton','actual_clinton']
+).interactive()
+
+if st.checkbox('View Scatter-matrix'):
+    st.altair_chart(scatter_matrix)
+### End Intro
+#####################################################
+
 full_training=full_training.drop(columns=["cycle"])
 full_training=full_training.drop(columns=["branch"])
 full_training=full_training.drop(columns=["type"])
@@ -55,7 +88,7 @@ full_training=full_training.drop(columns=["timestamp"])
 #full_training=full_training.drop(columns=["pred_trump"])
 #full_training=full_training.drop(columns=["pred_clinton"])
 
-#print(full_training)
+print(full_training)
 
 #setting the predictions
 full_training.loc[full_training["rawpoll_trump"] < full_training["rawpoll_clinton"], "pred_trump"] = 0.0
@@ -79,18 +112,33 @@ full_training.loc[(full_training["rawpoll_trump"] > full_training["rawpoll_clint
 full_training.loc[(full_training["rawpoll_trump"] == full_training["rawpoll_clinton"]) & (full_training["actual_trump"] > full_training["actual_clinton"]), "correctResult"] = 0.0
 full_training.loc[(full_training["rawpoll_trump"] == full_training["rawpoll_clinton"]) & (full_training["actual_trump"] < full_training["actual_clinton"]), "correctResult"] = 0.0
 
-training_target=full_training["rawpoll_trump"]
-reg = LinearRegression(fit_intercept=True, normalize=False)
-reg.fit(full_training, training_target)
+##########################################
+## section that shows some of the code used for the models
+# any code put into the echo is both printed to streamlit and executed as code
+st.subheader("regression models")
+# using echo() to view code in the app
+# echo() both outputs the code to streamlit and executes it
+with st.echo():
+    #Linear Regression 
+    training_target=full_training["rawpoll_trump"]
+    reg = LinearRegression(fit_intercept=True, normalize=False)
+    reg.fit(full_training, training_target)
+
 print(reg.score(full_training, training_target))
 print(full_training)
 
+#using echo to view code in the app
+with st.echo():
+    #graph probability code for Logistic Regression on Trump data
+    clf=LogisticRegression()
+    clf.fit(full_training[["rawpoll_trump"]], full_training[["correctResult"]])
+    x1 = np.linspace(4.5, 8.5, 1000).reshape(-1, 1)
+    y1=clf.predict_proba(x1)[:,1]
+##############################################################################
+
 
 #below is the graph output for the trump logistic regression results 
-clf=LogisticRegression()
-clf.fit(full_training[["rawpoll_trump"]], full_training[["correctResult"]])
-x1 = np.linspace(4.5, 8.5, 1000).reshape(-1, 1)
-y1=clf.predict_proba(x1)[:,1]
+
 #=clf.predict(full_training[["rawpoll_trump"]]) # here we are adding a column for the training_target data set where 
 #clf.score(training_target[["rawpoll_trump"]],  training_target["correctResult"]) # getting the score
 fig, (ax1) = plt.subplots(1, figsize=(10, 10))#this shows the plot
@@ -113,14 +161,34 @@ ax1.set_xlabel('rawpoll_trump',fontsize=16)
 ax1.set_ylabel('correctResult',fontsize=16)
 ax1.plot(x1, y1, color='green') #plots what x1 and y1 are set equal to
 ax1.grid(True)#shows the lines through the x and y values representing the axis
-plt.show()
+#plt.show()
 
-print(clf.score(full_training[["rawpoll_trump"]], full_training[["correctResult"]]))
+st.pyplot(fig) ## streamlit function to output matplotlib charts
+######################################################################################
 
+clf_score = clf.score(full_training[["pred_trump"]], full_training[["correctResult"]])
+print(clf_score)
 
-st.write(full_training)
+############
+#hopefully makes a cool looking line chart
+
+#reset index for use in graphs
+
+score_chart = alt.Chart(full_training.reset_index())
+
+###############################################
+## chart output using the altair library
+# pretty intuitive and quick to use
+st.altair_chart(score_chart.mark_point()
+.encode(alt.X('index'), alt.Y("correctResult",
+scale=alt.Scale(domain =[-1,2], clamp = True)),
+color = 'pred_trump',
+opacity=alt.value(0.5))
+.properties(width = 750,height = 250,)
+.interactive())
+################################################
+
+# output some scores to streamlit
 st.write('reg score = ', reg.score(full_training, training_target))
-
-
-
+st.write('clf model score = ',clf_score)
 
